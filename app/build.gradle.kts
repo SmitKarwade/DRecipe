2dd28f2824f8f85db30ee995900f51b91a6cbcbb
SmitKarwade <smitkarwade31@gmail.com>
2025-05-16 08:49:08 +0530

    Changed compileSDK (34 to 35)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.drecipe"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.drecipe"
         minSdk = 30
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
